b9abe25177e515303a054038c92c7bc22560a3cf
Bishesh <bishesh.sunam@gmail.com>
2021-03-02 21:20:23 +1300

    added login functionality with spotify token authorization

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import Login from './Components/Login';
+import { getTokenFromUrl } from './spotify';
 
 function App() {
+  const[token, setToken] = useState(null);
+
+  //run code once when app loads based on give condition 
+  useEffect(() => {
+    const _token = getTokenFromUrl();
+    console.log("I've a token", token);
+    //remove the token immediately after the authorization
+    window.location.hash = "";
+    if(_token){
+      setToken(_token);
+    }
+  }, [])
   return (
     <div className="app">
-     <h2>Spotify-Clone</h2>
+      {token ? (
+        <h2>I'm login in</h2>
+      ):(
+        <Login />
+      )}
+    
     </div>
   );
 }
